59cb5979233b68c60fc9b0444cf5eeb77cb80c34
Katarzyna ZuberSmith <zuberka@hotmail.com>
2018-10-25 23:25:59 +0200

    nav + logo

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        {/*<ImageLinkForm />
+        <FaceRecognation />*/}
+      </div>
+    );
+  }
+}
+
+export default App;
