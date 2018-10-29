9cc8066baebf53624b4406f751ee947a2b3ba560
Katarzyna ZuberSmith <zuberka@hotmail.com>
2018-10-29 23:43:27 +0100

    another componenets, API, Signin

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Clarifai from'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognation from './components/FaceRecognation/FaceRecognation';
+import Signin from './components/Signin/Signin';
 import './App.css';
-import Particles from 'react-particles-js';
+
+
+const app = new Clarifai.App({
+ apiKey: 'afe72595543c44e9a038185a55bde01e'
+});
 
 const particlesOptions = {
   particles: {
@@ -24,17 +32,70 @@ const particlesOptions = {
 
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin'
+    }
+  }
+
+calculateFaceLocation= (data) => {
+  const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+  const image = document.getElementById('inputImage');
+  const width = Number(image.width);
+  const height = Number(image.height);
+  //console.log(width, height);
+  return {
+    leftCol: clarifaiFace.left_col * width,
+    topRow: clarifaiFace.top_row * height,
+    rightCol: width - (clarifaiFace.right_col * width),
+    bottomCol: height - (clarifaiFace.bottom_row * height)
+  }
+}
+
+displayFaceBox = (box) => {
+  console.log(box);
+  this.setState({box: box});
+}
+
+onInputChange = (event) => {
+  this.setState({input: event.target.value});
+}
+
+onButtonSubmit = () => {
+  this.setState({imageUrl: this.state.input})
+  app.models
+  .predict(
+    Clarifai.FACE_DETECT_MODEL, 
+    this.state.input)
+  .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+  .catch(err => console.log(err));
+}
+
+onRouteChange = (route) => {
+  this.setState({route: route})
+}
+
+
   render() {
     return (
       <div className="App">
         <Particles className='particles'
           params={particlesOptions}
         />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm />
-        {/*<FaceRecognation />*/}
+        <Navigation onRouteChange={this.onRouteChange}/>
+        { this.state.route ==='signin' 
+          ? <Signin onRouteChange ={this.onRouteChange}/>
+          : <div>
+              <Logo />
+              <Rank />
+              <ImageLinkForm  onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+              <FaceRecognation box={this.state.box} imageUrl={this.state.imageUrl}/>
+            </div>
+        }
       </div>
     );
   }
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -2,16 +2,16 @@ import React from 'react';
 import './ImageLinkForm.css'
 
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
 	return (
 		<div >
 		  <p className='f3'>
-		   {'This Magic Brain will detect faces in your pictures. Give it a try.'}
+		   {'This Magic Brain will detect faces in your pictures. Give it a try!'}
 		  </p>
 		  <div className='center'>
 		    <div className='form center pa4 br3 shadow-5'>
-		      <input className='f4 pa2 w-70 br3 center' type='text' />
-		      <button className='w-30 grow f4 link ph3 br3 dib white bg-orange'>Detect</button>
+		      <input className='f4 pa2 w-70 br3 center' placeholder='enter your image URL here' type='text' onChange={onInputChange}/>
+		      <button className='w-30 grow f4 link ph3 br3 dib white bg-orange' onClick={onButtonSubmit}>Detect</button>
 		    </div>
 		  </div>
 		</div>  
