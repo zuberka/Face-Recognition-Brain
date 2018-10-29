64b08065a63533b1f8bacb662ec9cc179592b705
Katarzyna ZuberSmith <zuberka@hotmail.com>
2018-10-29 23:51:14 +0100

    another componenets, API, Signin

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -20,7 +20,7 @@ const Signin = ({ onRouteChange }) => {
 		    </fieldset>
 		    <div>
 		        <input 
-		          onClick={onRouteChange}
+		          onClick={() => onRouteChange('home')}
 		          className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
 		          type="submit" 
 		          value="Sign in" />
