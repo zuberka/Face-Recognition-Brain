6250c964fbdb17dba109ca0f10371253b580a2e4
Katarzyna ZuberSmith <zuberka@hotmail.com>
2018-10-31 00:08:56 +0100

    website routing

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import FaceRecognation from './components/FaceRecognation/FaceRecognation';
 import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import './App.css';
 
 
@@ -38,46 +39,52 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
-calculateFaceLocation= (data) => {
-  const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
-  const image = document.getElementById('inputImage');
-  const width = Number(image.width);
-  const height = Number(image.height);
-  //console.log(width, height);
-  return {
-    leftCol: clarifaiFace.left_col * width,
-    topRow: clarifaiFace.top_row * height,
-    rightCol: width - (clarifaiFace.right_col * width),
-    bottomCol: height - (clarifaiFace.bottom_row * height)
+  calculateFaceLocation= (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    //console.log(width, height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomCol: height - (clarifaiFace.bottom_row * height)
+    }
   }
-}
 
-displayFaceBox = (box) => {
-  console.log(box);
-  this.setState({box: box});
-}
+  displayFaceBox = (box) => {
+    //console.log(box);
+    this.setState({box: box});
+  }
 
-onInputChange = (event) => {
-  this.setState({input: event.target.value});
-}
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
 
-onButtonSubmit = () => {
-  this.setState({imageUrl: this.state.input})
-  app.models
-  .predict(
-    Clarifai.FACE_DETECT_MODEL, 
-    this.state.input)
-  .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
-  .catch(err => console.log(err));
-}
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL, 
+        this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
+  }
 
-onRouteChange = (route) => {
-  this.setState({route: route})
-}
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
 
 
   render() {
@@ -86,15 +93,22 @@ onRouteChange = (route) => {
         <Particles className='particles'
           params={particlesOptions}
         />
-        <Navigation onRouteChange={this.onRouteChange}/>
-        { this.state.route ==='signin' 
-          ? <Signin onRouteChange ={this.onRouteChange}/>
-          : <div>
+        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
+        { this.state.route ==='home' 
+          ? <div>
               <Logo />
               <Rank />
-              <ImageLinkForm  onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+              <ImageLinkForm  
+                onInputChange={this.onInputChange} 
+                onButtonSubmit={this.onButtonSubmit}
+              />
               <FaceRecognation box={this.state.box} imageUrl={this.state.imageUrl}/>
             </div>
+          : (
+              this.state.route === 'signin'
+              ? <Signin onRouteChange ={this.onRouteChange}/>
+              : <Register onRouteChange ={this.onRouteChange}/>
+            )
         }
       </div>
     );
